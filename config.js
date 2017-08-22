44ec8854c69705ab48057948b77280e52fff9402
AJ <735267452@qq.com>
2017-08-22 21:43:49 +0800

    自定义房间选择

---
+++ config.js
@@ -0,0 +1,42 @@
+/**
+ * Created by Administrator on 2017/8/22.
+ */
+module.exports = {
+    interval : 5, // 每五分钟查询一次
+    queryConfig: {
+        // excludeRooms表示 这些房间我不要，放房间号 例如 ： ["1201", "1205"]  这样写表示这栋楼的1201房间和1205房间即使为空房也不要告诉我，也不要停止程序
+        // isQuery表示 是否查询该栋楼，如果为false表示不查询这栋楼（因为高新区公寓是男女分开楼住的，你是男生，查询女生楼空房间是没用的，租不了。女生同理。）
+        1: {  // 明珠东楼男生
+            isQuery: true,
+            excludeRooms: []
+        },
+        2: {  //创新A座
+            isQuery: true,
+            excludeRooms: []
+        },
+        3: { //创新B座
+            isQuery: true,
+            excludeRooms: []
+        },
+        4: {  //创新C座
+            isQuery: true,
+            excludeRooms: []
+        },
+        5: {  //皖水公寓1#
+            isQuery: true,
+            excludeRooms: []
+        },
+        6: {  //皖水公寓2#
+            isQuery: true,
+            excludeRooms: []
+        },
+        7: { //皖水公寓 综合楼东
+            isQuery: true,
+            excludeRooms: []
+        },
+        8: { //皖水公寓 综合楼西
+            isQuery: true,
+            excludeRooms: []
+        }
+    }
+};
--- index.js
+++ index.js
@@ -9,7 +9,8 @@ var moment = require("moment");
 
 let request = require('request');
 let isPolling = true;
-let interval = 5; // 5 minutes
+let config = require("./config");
+let interval = config.interval; // 5 minutes
 
 
 function alert() {
@@ -27,57 +28,64 @@ function alert() {
     // 明珠公寓
     var list = [
         {
-            desc : "明珠东楼男生",
+            id: 1,
+            desc: "明珠东楼男生",
             url: "http://36.7.87.172:8088/online/roomResource.xp?action=formList1",
             data: {
                 "code": "01",
                 "buildingCode": "0011502190427062JFn"  // 明珠东楼男生
             }
-        }
-        , {
-            desc : "创新A座",
+        }, {
+            id: 2,
+            desc: "创新A座",
             url: "http://36.7.87.209:8088/online/roomResource.xp?action=formList1",
             data: {
                 "code": "01",
                 "buildingCode": "0011502154277240phA"  //创新A座
             }
         }, {
-            desc : "创新B座",
+            id: 3,
+            desc: "创新B座",
             url: "http://36.7.87.209:8088/online/roomResource.xp?action=formList1",
             data: {
                 "code": "01",
                 "buildingCode": "0011502154321961NnF"  //创新B座
             }
         }, {
-            desc : "创新C座",
+            id: 4,
+            desc: "创新C座",
             url: "http://36.7.87.209:8088/online/roomResource.xp?action=formList1",
             data: {
                 "code": "01",
                 "buildingCode": "0011502154352377wUW"  //创新C座
             }
         }, {
-            desc : "皖水公寓1#",
+            id: 5,
+            desc: "皖水公寓1#",
             url: "http://117.71.57.99:9080/online/roomResource.xp?action=formList1",
             data: {
                 "code": "01",
                 "buildingCode": "0011449816806945psc"  //皖水公寓1#
             }
         }, {
-            desc : "皖水公寓2#",
+            id: 6,
+            desc: "皖水公寓2#",
             url: "http://117.71.57.99:9080/online/roomResource.xp?action=formList1",
             data: {
                 "code": "01",
                 "buildingCode": "0011449816830250MuI"  //皖水公寓2#
             }
         }, {
-            desc : "皖水公寓 综合楼东",
+            id: 7,
+            desc: "皖水公寓 综合楼东",
             url: "http://117.71.57.99:9080/online/roomResource.xp?action=formList1",
             data: {
                 "code": "01",
                 "buildingCode": "0011449816876736sfx"  //皖水公寓 综合楼东
             }
         }, {
-            desc : "皖水公寓 综合楼西",
+            id: 8,
+            desc: "皖水公寓 综合楼西",
             url: "http://117.71.57.99:9080/online/roomResource.xp?action=formList1",
             data: {
                 "code": "01",
@@ -97,8 +105,10 @@ function alert() {
     function loop() {
         if (!isPolling) return;
         console.log("==============" + moment().format('LTS') + "====================");
-        list.forEach(function (config) {
-            run(config);
+        list.forEach(function (map) {
+            if (config.queryConfig[map.id].isQuery) {
+                run(map);
+            }
         })
     }
 
@@ -111,7 +121,7 @@ function alert() {
             function (error, response, body) {
                 if (!error && response.statusCode == 200) {
                     let json = JSON.parse(body);
-                    oo(json.list);
+                    oo(json.list, map);
 
                     if (findList.length > 0) {
                         isPolling = false;
@@ -128,12 +138,20 @@ function alert() {
             }
         );
 
-        function oo(vo) {
+        function oo(vo, map) {
             for (var key in vo) {
                 for (var j = 0; j < vo[key].length; j++) {
-                    var status = (vo[key][j]['status'] + "");
+                    let status = (vo[key][j]['status'] + "");
+                    let roomName = vo[key][j]['roomName'] + "";
+                    // if the room is empty
                     if (status == "02" || status == "2") {
-                        findList.push(vo[key][j]['roomName']);
+                        let excludeRooms = config.queryConfig[map.id].excludeRooms.map(function (item) {
+                            return item + "";
+                        });
+
+                        if (excludeRooms.indexOf(roomName) == -1) {
+                            findList.push(vo[key][j]['roomName']);
+                        }
                     }
                 }
             }
